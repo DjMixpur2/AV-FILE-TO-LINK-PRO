d46bc85f5687454608ef9dcb12afc76c30180cf2
DjMixpur2 <djmixpur2@gmail.com>
2025-10-02 19:17:40 +0530

    Update info.py
    Signed-off-by: DjMixpur2 <djmixpur2@gmail.com>

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ BIN_CHANNEL = int(environ.get("BIN_CHANNEL", '-1002479847516'))  # File storage 
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", '-1002479847516'))  # General log channel
 PREMIUM_LOGS = int(environ.get("PREMIUM_LOGS", '-1002479847516'))  # Premium user actions log
 VERIFIED_LOG = int(environ.get('VERIFIED_LOG', '-1002479847516'))  # Verified user actions log
-SUPPORT_GROUP = int(environ.get("SUPPORT_GROUP", ""))
+SUPPORT_GROUP = int(environ.get("SUPPORT_GROUP", "-1002389029976"))
 
 # add admin IDs 11111 2222 3333 and add auth channel IDs -100XXX -100XXX -100XXX
 ADMINS = list(map(int, environ.get('ADMINS', '770434685').split()))  # List of admin user IDs
