f4422a3c23a2edd452fc2835d101b18645379f0e
DjMixpur2 <djmixpur2@gmail.com>
2025-10-02 20:16:24 +0530

    Update info.py
    Signed-off-by: DjMixpur2 <djmixpur2@gmail.com>

--- info.py
+++ info.py
@@ -83,7 +83,7 @@ else:
     ON_HEROKU = False
 
 # 🌐 Server Settings
-PORT = int(getenv('PORT', '7023'))  # Port for web server
+PORT = int(getenv('PORT', '8080'))  # Port for web server
 NO_PORT = str(getenv("NO_PORT", False)).lower() in ("true", "1", "yes")  # Disable port in URL
 HAS_SSL = str(getenv("HAS_SSL", False)).lower() in ("true", "1", "yes")  # Use HTTPS if True
 BIND_ADDRESS = getenv("WEB_SERVER_BIND_ADDRESS", "127.0.0.1")  # Server bind address
